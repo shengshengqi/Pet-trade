d46921c0d464614bc2f0413595663dbf36d0414a
shengshengqi <1296813320@qq.com>
2019-06-05 20:06:41 +0800

    通过主人id找到他的宠物

---
+++ petsails/api/controllers/user/search.js
@@ -0,0 +1,38 @@
+module.exports = {
+    friendlyName: 'search pets by ownerID',
+
+    description: '根据主人id查找宠物',
+
+    inputs: {
+        id: {
+            description: '主人id',
+            type: 'string',
+            required: true,
+        }
+    },
+
+    exits: {
+        success: {
+            statusCode: 200,
+        },
+        notFound: {
+            statusCode: 404,
+        }
+    },
+
+    fn: async function (inputs, exits) {
+        if(this.req.session.userId){
+            let info = await Pet.find({
+                or: [
+                    { petVariety: { 'owner': inputs.id} },
+                ]
+            })
+
+            if (info[0]) {
+                return exits.success({ info });
+            } else {
+                return exits.notFound();
+            }
+    }
+    }
+};
